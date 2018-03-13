0c0320b27ebda28f605cfa81be3a345cb1332dfc
Goronmon <dev@tsgagnon.com>
2018-03-13 10:13:18 -0400

    Various fixes

--- eventPage.js
+++ eventPage.js
@@ -1,62 +1,64 @@
-chrome.windows.onFocusChanged.addListener(function() {
-    chrome.tabs.query({'active': true}, function (tabs) {
-        var url = new URL(tabs[0].url);
+chrome.windows.onFocusChanged.addListener(function () {
+    chrome.tabs.query({ 'active': true, 'currentWindow': true }, function (tabs) {
+        if (tabs[0]) {
+            var url = new URL(tabs[0].url);
 
-        chrome.storage.sync.get({
-            blacklist: []
-        }, function (items) {
-            if (items.blacklist.includes(url.hostname)) {
-                // Get alarm
-                chrome.alarms.get('RefocusAlarm', function (alarm){
-                    // If alarm doesn't exist create it
-                    if (alarm){
-                        // Do nothing
-                    }
-                    else {
-                        if (localStorage.getItem("refocusAlarmScheduledTime") !== null) {
-                            var previousScheduledTime = parseInt(localStorage.refocusAlarmScheduledTime, 10);
-                            if (isNaN(previousScheduledTime)) {
-                                startAlarm();
-                            }
-                            else {
-                                var previousPauseTime = parseInt(localStorage.refocusAlarmPauseTime, 10);
-                                if (isNaN(previousPauseTime)) {
+            chrome.storage.sync.get({
+                blacklist: []
+            }, function (items) {
+                if (items.blacklist.includes(url.hostname)) {
+                    // Get alarm
+                    chrome.alarms.get('RefocusAlarm', function (alarm) {
+                        // If alarm doesn't exist create it
+                        if (alarm) {
+                            // Do nothing
+                        }
+                        else {
+                            if (localStorage.getItem("refocusAlarmScheduledTime") !== null) {
+                                var previousScheduledTime = parseInt(localStorage.refocusAlarmScheduledTime, 10);
+                                if (isNaN(previousScheduledTime)) {
                                     startAlarm();
                                 }
                                 else {
-                                    var currentTime = Date.now();
-                                    var newScheduledTime = (currentTime - previousPauseTime) + previousScheduledTime;
-                                    
-                                    clearAlarm();
-                                    chrome.alarms.create('RefocusAlarm', {when: newScheduledTime});
+                                    var previousPauseTime = parseInt(localStorage.refocusAlarmPauseTime, 10);
+                                    if (isNaN(previousPauseTime)) {
+                                        startAlarm();
+                                    }
+                                    else {
+                                        var currentTime = Date.now();
+                                        var newScheduledTime = (currentTime - previousPauseTime) + previousScheduledTime;
+
+                                        clearAlarm();
+                                        chrome.alarms.create('RefocusAlarm', { when: newScheduledTime });
+                                    }
                                 }
                             }
+                            else {
+                                startAlarm();
+                            }
+                        }
+                    });
+
+                }
+                else {
+                    // Pauser timer
+                    chrome.alarms.get('RefocusAlarm', function (alarm) {
+                        // If alarm doesn't exist create it
+                        if (alarm) {
+                            localStorage.removeItem('refocusAlarmScheduledTime');
+                            localStorage.removeItem('refocusAlarmPauseTime');
+
+                            localStorage.refocusAlarmScheduledTime = alarm.scheduledTime;
+                            localStorage.refocusAlarmPauseTime = Date.now();
+                            chrome.alarms.clear("RefocusAlarm");
                         }
                         else {
-                            startAlarm();
+                            // Do nothing
                         }
-                    }
-                });
-    
-            }
-            else {
-                // Pauser timer
-                chrome.alarms.get('RefocusAlarm', function (alarm){
-                    // If alarm doesn't exist create it
-                    if (alarm){
-                        localStorage.removeItem('refocusAlarmScheduledTime');
-                        localStorage.removeItem('refocusAlarmPauseTime');
-    
-                        localStorage.refocusAlarmScheduledTime = alarm.scheduledTime;
-                        localStorage.refocusAlarmPauseTime = Date.now();
-                        chrome.alarms.clear("RefocusAlarm");
-                    }
-                    else {
-                        // Do nothing
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
+        }
     });
 });
 
@@ -91,15 +93,15 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
                     }
                 });
             });
-        });
 
-        chrome.storage.sync.get("refocusCloseTab", function (closeTabs) {
-            if (closeTabs) {
-                chrome.tabs.remove(tabsToClose);
-            }
-            else {
-                alert("Time's UP!");
-            }
+            chrome.storage.sync.get("refocusCloseTab", function (closeTabs) {
+                if (closeTabs) {
+                    chrome.tabs.remove(tabsToClose);
+                }
+                else {
+                    alert("Time's UP!");
+                }
+            });
         });
     })
 });
